bf7e0749ee43cf3d00daba02d4e48421cb29f1b8
fedsimon <claussimon@gmail.com>
2013-02-10 17:29:32 -0600

    Update aux_ProDy.py

--- aux_ProDy.py
+++ aux_ProDy.py
@@ -6,14 +6,14 @@ acc = pr.fetchPDB('1ACC')
 pacc = pr.parsePDB(acc) #http://www.csb.pitt.edu/ProDy/examples/proteins/parsepdb.html#parsepdb for options
 
 def neighborhood(iterable):
-    iterator = iter(iterable)
-    prev = None
-    item = iterator.next()  # throws StopIteration if empty.
-    for next in iterator:
-        yield (prev,item,next)
-        prev = item
-        item = next
-    yield (prev,item,None)
+	iterator = iter(iterable)
+	prev = None
+	item = iterator.next()  # throws StopIteration if empty.
+	for next in iterator:
+		yield (prev,item,next)
+		prev = item
+		item = next
+	yield (prev,item,None)
 
 for prev,item,next in neighborhood(pacc):
 	print prev, item, next
