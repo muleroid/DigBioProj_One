fa9682c7231bff09411703f19f42c53f9f9c9349
Shen Gong <shngong@gmail.com>
2013-02-24 15:49:12 -0600

    angle code
    added functionality to calculate angle components

--- rewrite_shen.py
+++ rewrite_shen.py
@@ -1,5 +1,7 @@
 # Rewrite Shen's parse_pdb.py using Prody
 
+import math
+import numpy as np
 import prody as pr
 
 #______________________________________________________________
@@ -17,6 +19,35 @@ def getHforAtom(aParsedProPDB, anAtom):
 	#hydrogens = aParsedPDBfile.select('element H')
 	#hydrogens.select('')
 
+# given atom structures for N, C, O, return position of hydrogen atom
+def getHPos(nAtom,cAtom,oAtom):
+        nCoords = nAtom.getCoords()
+        cCoords = cAtom.getCoords()
+        oCoords = oAtom.getCoords()
+        top = np.subract(cCoords,oCoords)
+        bot = np.linalg.norm(top)
+        hCoords = np.add(nCoords, np.divide(top,bot))
+        return hCoords
+
+# two functions to calculate the hydrogen bond angles beta and gamma
+def getBeta(cAtom,oAtom,hAtom):
+        cCoords = cAtom.getCoords()
+        oCoords = oAtom.getCoords()
+        hCoords = hATom.getCoords()
+        proj = np.dot(np.subtract(hCoords,oCoords),np.subtract(oCoords,cCoords))
+        vector = np.multiply(np.subtract(oCoords,cCoords),proj)
+        # now calculate the angle between vector and O-H vector using w/e
+        # function we wrote
+
+def getGamma(
+        cCoords = cAtom.getCoords()
+        oCoords = oAtom.getCoords()
+        hCoords = hATom.getCoords()
+        proj = np.dot(np.subtract(hCoords,oCoords),np.subtract(oCoords,cCoords))
+        vector = np.multiply(np.subtract(oCoords,cCoords),proj)
+        inplane = np.linalg.cross(np.subtract(vector,oCoords),np.subtract(hCoords,oCoords))
+        # calculate angle between vector and inplane
+        
 pdbFile = '/Users/fsimon/Google Drive/School/Winter_13/Digital Biology/Project/DigBioProj_One/pdbs/1A23_A.pdb'
 
 aParsedPDBfile = pr.parsePDB(pdbFile, model=1, secondary=True, chain='A', altLoc=False)
